9f8e414ef0099a549edb3bab11ec03a4207084ac
dadan <dadanhidyt@gmail.com>
2022-08-05 07:17:32 -0700

    Library cookie

--- src/Libs/Cookie.php
+++ src/Libs/Cookie.php
@@ -1,17 +1,31 @@
 <?php
 namespace Libs;
 class Cookie {
-    public function set($key, $value) {
-
+    public function _setCookie($name,$value,$expires,$path,$secure,$http_only) {
+        if(is_array($name)) {
+          $cookie_name = $name['name'];
+          $value = $name['name'];
+          $expires = $name['expires'] ?? $expires;
+          $path = $name['path'] ?? $path;
+          $secure = $name['secure'] ?? $secure;
+          $http_only = $name['http_only'] ?? $http_only;
+        }
+        setcookie($cookie_name, $value, $expires, $path, $secure,$http_only);
     }
-    public function get() {
-
+    public function set($name,$value = '',$expires = 0,$path = '',$secure = false,$http_only = true) {
+        $this->_setCookie($name,$value,$expires,$path,$secure,$http_only);
+    }
+    public function get($name) {
+        if ($this->has($name)) {
+            return $_COOKIE[$name];
+        }
     }
     public function has($name) {
-
+        return $_COOKIE[$name] ?? false;
     }
     public function destroy($name) {
-
+        unset($_COOKIE[$name]);
     }
-    
+
 }
+?>
