de55cabed0346e1925a43dc13703da0fdd4f1743
raflyfrmnsyh <raflyfirmansyah017@gmail.com>
2022-08-05 23:18:20 +0700

    Menambahkan Navbar Client-side

--- view/index.php
+++ view/index.php
@@ -1,3 +1,36 @@
-<div class="container">
-	asdhaj
-</div>
+<nav class="navbar navbar-desk navbar-light">
+	<div class="container">
+		<a class="navbar-brand" href="#">
+			<img src="assets/images/client-side/logo-app.svg" alt="logo-app">
+		</a>
+		
+		<div class="navbar-menu d-flex align-items-center">
+			<a href="#" class="nav-link active">
+				<i class='bx bx-home-alt' ></i>
+				<span>Beranda</span>
+			</a>
+			<a href="#" class="nav-link">
+				<span>Berita</span>
+			</a>
+			<a href="#" class="nav-link">
+				<span>Trending</span>
+			</a>
+			<a href="#" class="nav-link">
+				<span>Notifikasi</span>
+			</a>
+
+			<div class="search-box d-flex align-items-center">
+				<div class="search-ico d-flex align-items-center justify-content-center">
+					<i class='bx bx-search' ></i>
+				</div>
+				
+				<input type="text" class="input-search" placeholder="Cari sesuatu...">
+			</div>
+			<a href="#" class="login-bx btn">
+				Login
+			</a>
+		</div>
+
+
+	</div>
+</nav>
