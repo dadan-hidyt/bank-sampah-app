7dbe126246fd6a89a769ada74453db24499cd803
dadan <dadanhidyt@gmail.com>
2022-08-04 23:53:57 -0700

    perubahan pada halaman penjualan.php

--- view/pages/penjualan.php
+++ view/pages/penjualan.php
@@ -36,6 +36,17 @@
                 <a href="">DETAIL</a>
             </td>
         </tr>
+        <tr>
+            <td>3</td>
+            <td>TRX-JL-3444234-2021-2-2</td>
+            <td>@{echo date('h:i:s d-m-Y')}</td>
+            <td>WENDI SEPARATOR</td>
+            <td>+3.000</td>
+            <td>
+                <a href="">CETAK</a> - 
+                <a href="">DETAIL</a>
+            </td>
+        </tr>
        </tbody>
     </tr>
 </table>
