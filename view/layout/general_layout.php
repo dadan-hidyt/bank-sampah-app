4296b53324079c5bb69aca25508d7841a5959b06
raflyfrmnsyh <raflyfirmansyah017@gmail.com>
2022-08-05 21:51:45 +0700

    Menambahkan CDN boxicons dan fonts

--- view/layout/general_layout.php
+++ view/layout/general_layout.php
@@ -7,6 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @include('part/_head')
     <title>%title%</title>
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/style.css?v=<?= time()?>">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
 
 <body <?= set_body_class($body_class); ?>>
