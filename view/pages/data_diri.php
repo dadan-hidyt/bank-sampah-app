69b92d53c8bf5f3e6c98eb517552fba7e7833994
ahidyt <cemilcemil040704@gmail.com>
2022-08-09 13:36:15 +0700

    finish hlaman profile

--- view/pages/data_diri.php
+++ view/pages/data_diri.php
@@ -6,32 +6,91 @@
         </div>
         <div class="status ml-4 d-flex flex-column justify-content-center">
             <h1 class="font-weight-bold mb-2">Dadan Hidayat</h1>
-            <span class="font-weight-bold">Pekerja kreatif, Pemakar handal, dan Sekjen Utama Pemuda Pancasila</span>
+            <span class="font-weight-bold">Lihat informasi pribadimu di halaman ini</span>
         </div>
     </div>
     <h2 class="font-weight-bold mt-5">
         Informasi Pribadi
     </h2>
     <div class="mt-4">
-            <label for="" class="font-weight-bold">Nama lengkap</label>
+            <label for="" class="fs-16 font-weight-bold">Nama lengkap</label>
             <div class="p-3 border border-gray rounded-lg">
                 <span>Dadan Hidayat</span>
             </div>
     </div>
     <div class="d-flex mt-4">
         <div class="w-50 mr-2">
-            <label for="" class="font-weight-bold">Tanggal lahir</label>
+            <label for="" class="fs-16 font-weight-bold">Tempat lahir</label>
             <div class="p-3 border border-gray rounded-lg">
-                <span>Dadan</span>
+                <span>Markas PP</span>
             </div>
         </div>
         <div class="w-50 ml-2">
-            <label for="" class="font-weight-bold">Tempat lahir</label>
+            <label for="" class="fs-16 font-weight-bold">Tanggal lahir</label>
             <div class="p-3 border border-gray rounded-lg">
-                <span>Hidayat</span>
+                <span>18/12/2003</span>
             </div>
         </div>
     </div>
-    
+    <div class="d-flex mt-4 justify-content-between">
+        <div class="w-25">
+            <label for="" class="fs-16 font-weight-bold">Agama</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Islam KTP</span>
+            </div>
+        </div>
+        <div class="w-50">
+            <label for="" class="fs-16 font-weight-bold">Alamat</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Cadas pangeran, Sumedang</span>
+            </div>
+        </div>
+        <div class="w-10">
+            <label for="" class="fs-16 font-weight-bold">RT</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>002</span>
+            </div>
+        </div>
+        <div class="w-10">
+            <label for="" class="fs-16 font-weight-bold">RW</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>007</span>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex mt-4">
+        <div class="w-50 mr-2">
+            <label for="" class="fs-16 font-weight-bold">Kelurahan/Desa</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Desa Konoha</span>
+            </div>
+        </div>
+        <div class="w-50 ml-2">
+            <label for="" class="fs-16 font-weight-bold">Kecamatan</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Uchiha</span>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex mt-4">
+    <div class="w-50 mr-2">
+            <label for="" class="fs-16 font-weight-bold">Kabupaten</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Konoha Pojok</span>
+            </div>
+        </div>
+        <div class="w-50 ml-2">
+            <label for="" class="fs-16 font-weight-bold">Provinsi</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>Wakanda</span>
+            </div>
+        </div>
+    </div>
+    <div class="mt-4">
+            <label for="" class="fs-16 font-weight-bold">No. KTP</label>
+            <div class="p-3 border border-gray rounded-lg">
+                <span>321197802782466</span>
+            </div>
+    </div>
 
 </div>
