3233ad882599b3d8b53995da941679fb3d1597c2
dadan <dadanhidyt@gmail.com>
2022-08-24 22:11:16 -1100

    first

--- view/pages/produk_jual_view.php
+++ view/pages/produk_jual_view.php
@@ -1,1 +1,282 @@
-<?php create_breadcrumb(['Produk Jual'])?>
+<?php create_breadcrumb(['Produk Jual']) ?>
+<div class="mt-2">
+    <div class="bg-white p-3 shadow-sm">
+        <div class="button-area">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Tambah</button>
+        </div>
+    </div>
+    <div class="mt-3 bg-white shadow-sm">
+            <table id="tabel-barang" class="table text-center table-hover table-responsive table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAMA</th>
+                        <th>SATUAN</th>
+                        <th>HARGA SEKARANG</th>
+                        <th>HARGA KEMARIN</th>
+                        <th>DESKRIPSI</th>
+                        <th>ACT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>Sabun ayam</td>
+                        <td>KILO</td>
+                        <td>RP. 29.000</td>
+                        <td>0</td>
+                        <td>odio expedita corporis quas repellendus earum exercitationem ab sunt, perferendis iste. Minima, modi eum!</td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                            <button>Update harga</button>
+                        </td>
+                    </tr>
+                </tbody>
+                </tr>
+            </table>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+            <div class="form-group">
+                <label for="nama_produk">Nama Produk</label>
+                <input type="text" class='form-control' id='nama_produk' name='nama'>
+            </div>
+            <div class="form-group">
+                <label for="harga">Harga</label>
+                <input type="text" class='form-control' id='nama_produk' name='harga'>
+            </div>
+            <div class="form-group">
+                <label for="satuan">Satuan</label>
+                <select class="form-control" name="satuan" id="satuan">
+                    <option value="lusin">Lusin</option>
+                    <option value="pcs">pcs</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="deskripsi">Deskripsi</label>
+                <textarea name="" id="" cols="30" rows="4" class="form-control"></textarea>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">SIMPAN</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end modal -->
+<script>
+    window.onload = function() {
+        $('#tabel-barang').DataTable({});
+    }
+</script>
